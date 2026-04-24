55299c0c6447d11309f6818dca01a80328874013
md-moynul <mdmoynulislam58995@gmail.com>
2026-04-25 01:00:02 +0600

    add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/shared/Header";
-import Navbar from "@/components/shared/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "-font-poppins ",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight : ["400","500","600","700","800"] 
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -23,10 +17,10 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={` h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <main>
+        <main className={`${poppins.className}`}>
           {children}
         </main>
       </body>
